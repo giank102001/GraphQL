7f13d6442c040506770b43e10f0b0b2e49ef4001
giank102001 <giancarlonava94@gmail.com>
2022-07-31 19:16:34 -0500

    c

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-// Load config
-dotenv.config({ path: "variables.env" });
+const mongoose = require('mongoose')
+require('dotenv').config({path: 'variables.env'})
 
 const conectarDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("db conectada");
-  } catch (error) {
-    console.log("Hubo un error: ", error);
-    process.exit(1); // detener la app
-  }
-};
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('db conectada')
+    } catch (error) {
+        console.log('Hubo un error: ', error)
+        process.exit(1) // detener la app
+    }
+}
 
 module.exports = conectarDB;
